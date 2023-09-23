684f8aacc7270fba7b99f18569f21bf925a787ee
keepkilay <alekseevvobis@mail.ru>
2023-09-23 17:51:40 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const sayHello = (
+	name = 'Иван',
+  surname = 'Иванов',
+  age = 10,
+  greeting = 'Привет, ',
+ ) => (
+ 	`${greeting}${name} ${surname}, тебе ${age} лет.`
+);
+
+alert( sayHello() );
+alert( sayHello('Дима') );
+alert( sayHello('Евгений', 'Петров', 25, 'Здравствуйте, ') );
+
